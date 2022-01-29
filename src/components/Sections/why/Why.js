da8b15e47183f07f95d2060ff60eb71b54b84735
Sergio Escobar <93834310+sergioesc@users.noreply.github.com>
2022-01-29 11:26:59 -0300

    version 0.2.1

--- src/components/Sections/why/Why.js
+++ src/components/Sections/why/Why.js
@@ -9,7 +9,7 @@ const What = ()=>{
                     <h2>Why choose us?</h2>
                     <p id="section-what-text-delete"> It’s our attention to the small stuff, scheduling of timelines and keen project management that makes us stand out from the rest. We are creative, while keeping a close eye on the calendar and your budget.</p>
                     <p>Our prices are competitive and fair. There are no surprise bills. Any unexpected or additional expenses must be pre-approved by you. That’s how we would like to be treated, and that is how our clients are treated.</p>
-                    <Link to="/contact">
+                    <Link to="/gimenez/contact">
                     Contact
                     </Link>
                 </div>
